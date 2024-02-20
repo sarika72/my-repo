b2801cefabfe796c459feef2ec03d90b8f65f73c
sarika72 <160009606+sarika72@users.noreply.github.com>
2024-02-20 14:34:13 +0530

    count.tf

--- 19-2/loops/count.tf
+++ 19-2/loops/count.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-2"
 }
-resource "aws-instance" "instance"{
+resource "aws_instance" "instance"{
     count = "4"
     ami = "ami-05fb0b8c1424f266b"
     instance_type = "t2.micro"
